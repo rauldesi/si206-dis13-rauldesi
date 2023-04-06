0b5c2211dc369497984afbd3776348e080a97ce7
rauldesi <94874529+rauldesi@users.noreply.github.com>
2023-04-05 22:49:20 -0400

    done

--- discussion_13.py
+++ discussion_13.py
@@ -16,7 +16,17 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+
+    cur.execute("DROP TABLE IF EXISTS employees")
+    cur.execute("CREATE TABLE employees (" +
+                "employee_id INTEGER PRIMARY KEY, " +
+                "first_name TEXT, " +
+                "last_name TEXT, " +
+                "job_id INTEGER, " +
+                "hire_date TEXT, " +
+                "salary NUMERIC)")
+
+    conn.commit()
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,20 +37,59 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    
+    emp_dict = json.loads(file_data)
+
+    for emp in emp_dict:    
+        cur.execute("INSERT INTO employees " +
+                    "(employee_id, first_name, last_name, job_id, hire_date, salary)" +
+                    "VAlUES (?,?,?,?,?,?)",
+                    (emp['employee_id'], emp['first_name'], emp['last_name'], emp['job_id'], emp['hire_date'], emp['salary'])
+                    )
+    conn.commit()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    row = cur.execute("SELECT e.hire_date, j.job_title FROM employees e " +
+                       "JOIN jobs j " +
+                       "ON e.job_id = j.job_id " +
+                       "ORDER BY e.hire_date").fetchone()
+    return row[1]
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    rows = cur.execute("SELECT e.first_name, e.last_name FROM employees e " +
+                       "JOIN jobs j on e.job_id = j.job_id " +
+                       "WHERE e.salary < j.min_salary " +
+                       "OR e.salary > j.max_salary").fetchall()
+    return rows
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
-    pass
+    rows = cur.execute("SELECT j.job_title, e.salary " +
+                       "FROM employees e " +
+                       "JOIN jobs j " + 
+                       "ON e.job_id = j.job_id").fetchall()
+    x = []
+    y = []
+    for row in rows:
+        x.append(row[0])
+        y.append(row[1])
+    plt.scatter(x, y)
+
+    jobs = cur.execute("SELECT job_title, min_salary, max_salary " +
+                       "FROM jobs").fetchall()
+    x1 = []
+    y1 = []
+    for job in jobs:
+        x1.append(job[0])
+        y1.append(job[1])
+        x1.append(job[0])
+        y1.append(job[2])
+    plt.scatter(x1, y1, color='red', marker='x')
+
+    plt.show()
 
 class TestDiscussion12(unittest.TestCase):
     def setUp(self) -> None:
@@ -75,6 +124,8 @@ def main():
 
     wrong_salary = (problematic_salary(cur, conn))
     print(wrong_salary)
+    
+    visualization_salary_data(cur, conn)
 
 if __name__ == "__main__":
     main()
